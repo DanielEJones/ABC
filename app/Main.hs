c4765c2b722be16ebaaab1f48e0967f271e06895
DanielEJones <djonesthe21st@gmail.com>
2026-08-13 14:20:56 +0800

    Added basic CLI parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,24 @@
 module Main (main) where
 
+import Control.Monad (forM_)
+import Control.Monad.Trans (liftIO)
+
+import Common.Command
+import Common.Query
+
+
+fetchSource :: FilePath -> Query String
+fetchSource path = cached dbSource path $ do
+  content <- liftIO (readFile path)
+  pure content
+
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  options <- getOptions
+  db <- emptyDatabase
+
+  forM_ (optFiles options) $ \filePath -> do
+    content <- runQuery (fetchSource filePath) db
+    putStrLn content
+
